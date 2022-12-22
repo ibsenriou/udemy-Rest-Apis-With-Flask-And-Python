f9842f19a9ef972ab2edd62cee268e28be45f6c4
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-12-22 14:50:49 -0300

    First commit!

---
+++ resources/tag.py
@@ -0,0 +1,90 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import StoreModel, TagModel, ItemModel
+from schemas import TagSchema, TagAndItemSchema
+
+
+blp = Blueprint("Tags", 'tags', description="Operations on tags")
+
+
+@blp.route("/store/<int:store_id>/tag")
+class TagsInStore(MethodView):
+    @blp.response(200, TagSchema(many=True))
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+
+        return store.tags.all()
+
+    @blp.arguments(TagSchema)
+    @blp.response(201, TagSchema)
+    def post(self, tag_data, store_id):
+        tag = TagModel(**tag_data, store_id=store_id)
+
+        try:
+            db.session.add(tag)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
+
+        return tag
+
+
+@blp.route("/item/<int:item_id>/tag/<string:tag_id>")
+class LinkTagsToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.append(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="Failed to link tag to item")
+
+        return tag
+
+    @blp.response(204, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="Failed to unlink tag from item")
+
+        return TagAndItemSchema().dump({"message": "Tag unlinked from item", "item": item, "tag": tag})
+
+
+@blp.route("/tag/<int:tag_id>")
+class Tag(MethodView):
+
+    @blp.response(200, TagSchema)
+    def get(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        return tag
+
+    @blp.response(
+        202,
+        description="Deletes a tag if no item is tagged with it.",
+        example={'message': 'Tag deleted'})
+    @blp.alt_response(404, description="Tag not found")
+    @blp.alt_response(409, description="Tag is linked to an item.")
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"message": "Tag deleted"}
+
+        abort(409, message="Tag is linked to an item.")
